99d5afc055d84b8920e4fdc65a5e2bacfd2d1b55
Josal <joyalmeida321@gmail.com>
2022-07-14 15:08:59 +0530

    completed apis and pagination

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Api, Resource
 
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import os
 
 from dotenv import load_dotenv
@@ -24,9 +25,17 @@ Images = mongo.db.images
 # API VIEWS
 
 @app.route('/',methods = ['GET'])
-def getImages():
+@app.route('/<int:page>',methods = ['GET'])
+def getImages(page=None):
 	# get all images - pagination
-	images = Images.find({}, {"ImgURL":1,})
+	perPage = 2
+	page_no = 0
+	if page != None:
+		page_no = page-1
+	count = Images.count_documents({}) / perPage
+	if count % perPage != 0:
+		count = count + 1
+	images = Images.find({}, {"ImgURL":1,}).sort("_id").skip(perPage * page_no).limit(perPage)
 	img_list = []
 	for i in list(images):
 		item = {
@@ -35,7 +44,7 @@ def getImages():
 		}
 		img_list.append(item)
 
-	return {"images" : img_list}, 200
+	return {"images" : img_list, "page_count" : int(count)}, 200
 
 
 
@@ -44,40 +53,62 @@ def uploadImage():
 	# upload Images
 	data = request.get_json()
 	item = {
-		"ImgName" : data["image_name"]
-		"ImgURL" : data["image_url"]
+		"ImgName" : data["image_name"],
+		"ImgURL" : data["image_url"],
 		"ImgDetails" : data["image_details"]
 	}
 	Images.insert_one(item)
-	return {"message": "Question added Successfully"}, 200
+	return {"message": "Image added Successfully"}, 200
 
 
 @app.route('/show/<string:id>',methods = ['GET'])
 def getImage(id):
 	# get image with id - 
-	image = Images.find({"_id": ObjectId(id)})
+	image = Images.find_one({'_id': ObjectId(id)})
+	if image == None:
+		return {"image": {}}, 200
 	item = {
 		"id" : str(image["_id"]),
-		"image_url": str(image["ImgURL"])
-		"image_details" : str(image["ImgDetails"])
+		"image_url": str(image["ImgURL"]),
+		"image_details" : str(image["ImgDetails"]),
 		"image_name": str(image["ImgName"])
 	}
 	return {"image" : item}, 200
 
-@app.route('/<string:id>/edit',methods = ['DELETE'])
-def deleteImage(id):
-	# delete image with id
-	pass
-
-@app.route('/delete/<string:id>',methods = ['PUT'])
+@app.route('/<string:id>/edit',methods = ['PUT'])
 def editImage(id):
 	# update image with id
-	pass
+	new_item = {}
+	data = request.get_json()
+	if "image_url" in data:
+		new_item["ImgURL"] = data["image_url"]
+	if "image_details" in data:
+		new_item["ImgDetails"] = data["image_details"]
+	if "image_name" in data:
+		new_item["ImgName"] = data["image_name"]
+	newvalues = { "$set": new_item }
+	Images.update_one({'_id': ObjectId(id)}, newvalues)
+	return {"message": "Image Updated"}, 200
 
-@app.route('/<string:name>',methods = ['PUT'])
-def searchImage(id):
+@app.route('/delete/<string:id>',methods = ['DELETE'])
+def deleteImage(id):
+	# delete image with id
+	Images.delete_one({"_id": ObjectId(id)})
+	return {'message': 'Image deleted successfully'},200
+
+@app.route('/<string:name>',methods = ['GET'])
+def searchImage(name):
 	# update image with id
-	pass
+	image = Images.find_one({"ImgName": str(name)})
+	if image == None:
+		return {"image" : {}}, 200
+	item = {
+		"id" : str(image["_id"]),
+		"image_url": str(image["ImgURL"]),
+		"image_details" : str(image["ImgDetails"]),
+		"image_name": str(image["ImgName"])
+	}
+	return {"image" : item}, 200
 
 
 if __name__ == '__main__':
